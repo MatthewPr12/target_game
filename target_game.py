934fce110b5723e8c3dd0350ea433aa99fc22042
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-10-29 15:14:40 +0300

    fixed get_pure_user_words() and pylint

--- target_game.py
+++ target_game.py
@@ -1,16 +1,16 @@
+"""target game: try to write all words possible from the letters given"""
 from typing import List
 import random
 import string
 
 
-def generate_grid() -> List[str]:
+def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
     """
     alpha = string.ascii_lowercase
     grid_lst = []
-    grid_lst1 = []
     for _ in range(3):
         grid_lst.append([])
 
@@ -18,10 +18,7 @@ def generate_grid() -> List[str]:
         for _ in range(3):
             i.append(random.choice(alpha).upper())
     print("Your letters are:", grid_lst)
-    for i in grid_lst:
-        for j in i:
-            grid_lst1.append(j.lower())
-    return grid_lst1
+    return grid_lst
 
 
 # def tuple_creator(lst):
@@ -35,6 +32,7 @@ def generate_grid() -> List[str]:
 
 
 def words_comparison(grid, word):
+    """checks if the word is possible to be written with the given letters"""
     for i in grid:
         if i in word:
             word.remove(i)
@@ -43,15 +41,15 @@ def words_comparison(grid, word):
     return False
 
 
-def get_words(f: str, letters: List[str]) -> List[str]:
+def get_words(file_name: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
 
-    with open(f, "r") as file1:
+    with open(file_name, "r") as file1:
         list_of_words = []
         for line in file1:
-            line = line[:-2]  # last two chr are \n
+            line = line[:-1].lower()  # last chr is \n
             if len(line) >= 4 and letters[4] in line:
                 # compare the grid and words
                 if words_comparison(letters, list(line)):
@@ -70,7 +68,8 @@ def get_user_words() -> List[str]:
     return lst_user_words
 
 
-def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+def get_pure_user_words(user_words: List[str], letters: List[str],
+                        words_from_dict: List[str]) -> List[str]:
     """
     (list, list, list) -> list
 
@@ -79,25 +78,31 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
     """
     pure_user_words = []
     for i in user_words:
-        if words_comparison(letters, list(i)) and (i not in words_from_dict):
-            pure_user_words.append()
+        i = i.lower()
+        if len(i) >= 4 and letters[4] in i:
+            if words_comparison(letters, list(i)) and (i not in words_from_dict):
+                pure_user_words.append(i)
     return pure_user_words
 
 
 def results():
     """puts the results into result.txt file"""
-    letters = generate_grid()
+    letters1 = generate_grid()
+    letters = []
+    for i in letters1:
+        for j in i:
+            letters.append(j.lower())
 
     with open("result.txt", "w") as result_file:
         input_words = get_user_words()
-        allowed_words = get_words("en.txt", letters)
+        allowed_words = get_words("en", letters)
         for i in input_words:
             if i in allowed_words:
                 result_file.write(i + "\n")
 
         result_file.write("The words not from dictionary:" + "\n")
         for i in input_words:
-            if not (i in allowed_words):
+            if i not in allowed_words:
                 result_file.write(i + "\n")
 
 
